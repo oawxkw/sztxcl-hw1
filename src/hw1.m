06c112fb3fb19f41c4f78b3a67366c11463a3d65
oawxkw <925243018@qq.com>
2019-03-01 17:21:01 +0800

    Update hw1.m

--- src/hw1.m
+++ src/hw1.m
@@ -33,18 +33,27 @@ fprintf("00%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));
 fprintf("Color table: %s\n",reshape(HEX(51:1078,:)',1,[]));
 
 % Pixel storage
-fprintf("Pixel storage: %s\n",reshape(HEX(1079:1134,:)',1,[]));ip = hex2dec("0436"); len = 8;
-fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));ip = ip + len; len = 8;
-fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));ip = ip + len; len = 8;
-fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));ip = ip + len; len = 8;
-fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));ip = ip + len; len = 8;
-fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));ip = ip + len; len = 8;
-fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));ip = ip + len; len = 8;
+fprintf("Pixel storage: %s\n",reshape(HEX(1079:1134,:)',1,[])); ip = hex2dec("0436"); len = 8;
+fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[])); ip = ip + len; len = 8;
+fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[])); ip = ip + len; len = 8;
+fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[])); ip = ip + len; len = 8;
+fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[])); ip = ip + len; len = 8;
+fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[])); ip = ip + len; len = 8;
+fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[])); ip = ip + len; len = 8;
 fprintf("0%sH: %s\n",dec2hex(ip),reshape(HEX((ip+1):(ip+len),:)',1,[]));
 
 %% hw1-2
 clc; clear; close all;
+lena = imread(".\pic\lena.bmp");
 
+subplot(241); imshow(lena,[0,255]); title('\fontsize{24}lena 8 bit'); lena = bitshift(lena,-1);
+subplot(242); imshow(lena,[0,127]); title('\fontsize{24}lena 7 bit'); lena = bitshift(lena,-1);
+subplot(243); imshow(lena,[0,  63]); title('\fontsize{24}lena 6 bit'); lena = bitshift(lena,-1);
+subplot(244); imshow(lena,[0,  31]); title('\fontsize{24}lena 5 bit'); lena = bitshift(lena,-1);
+subplot(245); imshow(lena,[0,  15]); title('\fontsize{24}lena 4 bit'); lena = bitshift(lena,-1);
+subplot(246); imshow(lena,[0,    7]); title('\fontsize{24}lena 3 bit'); lena = bitshift(lena,-1);
+subplot(247); imshow(lena,[0,    3]); title('\fontsize{24}lena 2 bit'); lena = bitshift(lena,-1);
+subplot(248); imshow(lena,[0,    1]); title('\fontsize{24}lena 1 bit');
 
 %% hw1-3
 clc; clear; close all;
